e0b77f5b4174cd695cbd45fb1c276c90f3869446
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-09-12 15:31:17 +0800

    on-progress: report viewer financial position

--- svelte/reportViewer_financialPosition.svelte
+++ svelte/reportViewer_financialPosition.svelte
@@ -3,7 +3,52 @@
 </script>
 
 <MainLayout>
-  <p>Report Viewer Financial Position</p>
+  <div>
+    <div>
+      <b>Benalu DEV</b>
+      <h3>Laporan Posisi Keuangan</h3>
+      <b>Periode Septermber 2024</b>
+    </div>
+    <div>
+      <div>
+        <div>
+        <div>
+          <b>Aktiva</b>
+          <ul>
+            <li>iojisdfj</li>
+            <li>sdfgsdfg</li>
+            <li>48hj8sdjfhgishjdio</li>
+          </ul>
+        </div>
+        <div>Total Aktiva</div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <b>Modal</b>
+            <ul>
+              <li>iojisdfj</li>
+              <li>sdfgsdfg</li>
+              <li>48hj8sdjfhgishjdio</li>
+            </ul>
+          </div>
+          <div>Total Modal</div>
+        </div>
+        <div>
+          <div>
+            <b>Hutang</b>
+            <ul>
+              <li>iojisdfj</li>
+              <li>sdfgsdfg</li>
+              <li>48hj8sdjfhgishjdio</li>
+            </ul>
+          </div>
+          <div>Total Hutang</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </MainLayout>
 
 <style>
