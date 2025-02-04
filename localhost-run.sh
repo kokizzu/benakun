b60f3d086cecbb94418cde029b69708c1e6a03c7
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2025-02-04 21:31:44 +0800

    fix type in .air.toml

--- localhost-run.sh
+++ localhost-run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
 ssh -R 80:localhost:1235 localhost.run | while read line; do
   echo "$line" | grep -oP 'https://\K[0-9a-zA-Z]+\.lhr\.life' \
     | xargs -I {} sed -i 's|^DOKU_NOTIFICATION_URL=.*|DOKU_NOTIFICATION_URL=https://{}/payments/notifications|' .env.override
